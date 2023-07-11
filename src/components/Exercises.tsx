d5da8f9eea8afcecb3872fd638ab9991a3662c94
Arashjp8 <arashjafarpour723@gmail.com>
2023-07-11 12:17:28 +0330

    exercise card added

--- src/components/Exercises.tsx
+++ src/components/Exercises.tsx
@@ -1,4 +1,13 @@
-import { Box, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  HStack,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 import { useEffect } from "react";
 import BodyPartsMenu from "./BodyPartsMenu";
 import Exercise from "../interfaces/Exercise";
@@ -19,9 +28,55 @@ const Exercises = ({ exercises, setExercises, bodyParts }: Props) => {
       border={"1px solid gold"}
     >
       <BodyPartsMenu bodyParts={bodyParts} />
-      {/* {exercises.map((exercise) => (
-        <Text>{exercise.name}</Text>
-      ))} */}
+      <Text>Showing Results</Text>
+      {exercises.map((exercise) => (
+        <Card
+          key={exercise.id}
+          size={"lg"}
+          width={"400px"}
+          height={"445px"}
+          marginBottom={5}
+          borderTop={"4px solid #ff2625"}
+          borderRadius={"none"}
+          transform={"scale(1, 1)"}
+          transition={"0.3s all ease-in-out"}
+          _hover={{ transform: "scale(1.1, 1.1)" }}
+        >
+          <CardHeader>
+            <Image
+              marginLeft={"50px"}
+              height={"226px"}
+              src={exercise.gifUrl}
+              alt={exercise.name}
+              loading="lazy"
+            />
+          </CardHeader>
+          <CardBody>
+            <HStack marginBottom={5}>
+              <Button
+                colorScheme="pink"
+                marginRight={2}
+                borderRadius={"full"}
+                fontSize={"14px"}
+                textTransform={"capitalize"}
+              >
+                {exercise.target}
+              </Button>
+              <Button
+                colorScheme="yellow"
+                borderRadius={"full"}
+                fontSize={"14px"}
+                textTransform={"capitalize"}
+              >
+                {exercise.bodyPart}
+              </Button>
+            </HStack>
+            <Text fontWeight={"600"} fontSize={"24px"}>
+              {exercise.name}
+            </Text>
+          </CardBody>
+        </Card>
+      ))}
     </Box>
   );
 };
