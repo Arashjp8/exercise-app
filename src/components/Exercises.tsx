a2dfe19f76be9dabe1af113e04fd42646d956377
Arashjp8 <arashjafarpour723@gmail.com>
2023-07-11 13:15:19 +0330

    some layout upgrades

--- src/components/Exercises.tsx
+++ src/components/Exercises.tsx
@@ -6,6 +6,7 @@ import {
   CardHeader,
   HStack,
   Image,
+  SimpleGrid,
   Text,
 } from "@chakra-ui/react";
 import { useEffect } from "react";
@@ -21,62 +22,61 @@ interface Props {
 const Exercises = ({ exercises, setExercises, bodyParts }: Props) => {
   useEffect(() => {}, []);
   return (
-    <Box
-      marginTop={10}
-      width={"80%"}
-      height={"500px"}
-      border={"1px solid gold"}
-    >
+    <Box marginTop={10} width={"80%"} border={"1px solid gold"}>
       <BodyPartsMenu bodyParts={bodyParts} />
-      <Text>Showing Results</Text>
-      {exercises.map((exercise) => (
-        <Card
-          key={exercise.id}
-          size={"lg"}
-          width={"400px"}
-          height={"445px"}
-          marginBottom={5}
-          borderTop={"4px solid #ff2625"}
-          borderRadius={"none"}
-          transform={"scale(1, 1)"}
-          transition={"0.3s all ease-in-out"}
-          _hover={{ transform: "scale(1.1, 1.1)" }}
-        >
-          <CardHeader>
-            <Image
-              marginLeft={"50px"}
-              height={"226px"}
-              src={exercise.gifUrl}
-              alt={exercise.name}
-              loading="lazy"
-            />
-          </CardHeader>
-          <CardBody>
-            <HStack marginBottom={5}>
-              <Button
-                colorScheme="pink"
-                marginRight={2}
-                borderRadius={"full"}
-                fontSize={"14px"}
-                textTransform={"capitalize"}
-              >
-                {exercise.target}
-              </Button>
-              <Button
-                colorScheme="yellow"
-                borderRadius={"full"}
-                fontSize={"14px"}
-                textTransform={"capitalize"}
-              >
-                {exercise.bodyPart}
-              </Button>
-            </HStack>
-            <Text fontWeight={"600"} fontSize={"24px"}>
-              {exercise.name}
-            </Text>
-          </CardBody>
-        </Card>
-      ))}
+      <Text fontWeight={"800"} fontSize={"40px"} marginY={10}>
+        Showing Results
+      </Text>
+      <SimpleGrid columns={3} spacing={"20px"}>
+        {exercises.map((exercise) => (
+          <Card
+            key={exercise.id}
+            size={"lg"}
+            width={"400px"}
+            height={"445px"}
+            marginBottom={5}
+            borderTop={"4px solid #ff2625"}
+            borderRadius={"none"}
+            transform={"scale(1, 1)"}
+            transition={"0.3s all ease-in-out"}
+            _hover={{ transform: "scale(1.1, 1.1)" }}
+          >
+            <CardHeader>
+              <Image
+                marginLeft={"50px"}
+                height={"226px"}
+                src={exercise.gifUrl}
+                alt={exercise.name}
+                loading="lazy"
+              />
+            </CardHeader>
+            <CardBody>
+              <HStack marginBottom={5}>
+                <Button
+                  colorScheme="pink"
+                  marginRight={2}
+                  borderRadius={"full"}
+                  fontSize={"14px"}
+                  textTransform={"capitalize"}
+                >
+                  {exercise.target}
+                </Button>
+                <Button
+                  colorScheme="yellow"
+                  borderRadius={"full"}
+                  fontSize={"14px"}
+                  textTransform={"capitalize"}
+                >
+                  {exercise.bodyPart}
+                </Button>
+              </HStack>
+              <Text fontWeight={"600"} fontSize={"24px"}>
+                {exercise.name}
+              </Text>
+            </CardBody>
+          </Card>
+        ))}
+      </SimpleGrid>
     </Box>
   );
 };
